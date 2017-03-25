6f19c32a33e3c92fa15e7c7a4af16a7967d9f506
firezenk <firezenk@gmail.com>
2017-03-25 11:41:40 +0100

    First property test

---
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -0,0 +1,27 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Jorge Garrido Oval, aka firezenk on 25/03/17.
+ * Project: com.karumi.maxibonskata
+ */
+
+@RunWith(JUnitQuickcheck.class)
+public class DeveloperProperties {
+
+    private static final String ANY_NAME = "Jorge";
+    private static final int ANY_NUMBER_OF_MAXIBONS = 1;
+
+    @Property public void theNumberOfMaxibonsAssignedIsPositiveOrZero(int numberOfMaxibons) {
+        Developer developer = new Developer(ANY_NAME, numberOfMaxibons);
+        System.out.println(developer);
+        assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+    }
+
+}
