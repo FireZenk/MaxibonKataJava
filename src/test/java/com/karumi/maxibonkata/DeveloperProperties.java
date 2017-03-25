8f753e7f53bf2b266fdeff2e0c5a6085e8f0d5bc
firezenk <firezenk@gmail.com>
2017-03-25 11:58:43 +0100

    Use DeveloperGenerator to pass random developers

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -1,5 +1,6 @@
 package com.karumi.maxibonkata;
 
+import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
@@ -18,8 +19,8 @@ public class DeveloperProperties {
     private static final String ANY_NAME = "Jorge";
     private static final int ANY_NUMBER_OF_MAXIBONS = 1;
 
-    @Property public void theNumberOfMaxibonsAssignedIsPositiveOrZero(int numberOfMaxibons) {
-        Developer developer = new Developer(ANY_NAME, numberOfMaxibons);
+    @Property public void theNumberOfMaxibonsAssignedIsPositiveOrZero(
+            @From(DevelopersGenerator.class) Developer developer) {
         System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
